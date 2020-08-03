bc59db79d8574f1f29b27365b7adc75ecb44eafb
Goce <ganastasovski@alarm.com>
2020-08-03 00:07:26 -0400

    connected the rest of the navigation

---
+++ app/src/main/java/com/alanford/carpediem/about/AboutFragment.kt
@@ -0,0 +1,10 @@
+package com.alanford.carpediem.about
+
+import androidx.fragment.app.Fragment
+import com.alanford.carpediem.R
+
+// About app section
+// Created by ganastasovski on 8/1/20.
+// Copyright (c) 2020 alanford. All rights reserved.
+//
+class AboutFragment : Fragment(R.layout.about_layout)
---
+++ app/src/main/java/com/alanford/carpediem/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.alanford.carpediem.settings
+
+import androidx.fragment.app.Fragment
+import com.alanford.carpediem.R
+
+// Settings Fragment, where you can modify your settings
+// Created by alanford on 8/2/20.
+// Copyright (c) 2020 ganastasovski. All rights reserved.
+//
+class SettingsFragment : Fragment(R.layout.settings_layout)
