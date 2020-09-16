6964743acab7aa4207fd40285961c18ede3e31e5
Goce <ganastasovski@alarm.com>
2020-09-15 21:15:27 -0400

    notifications support

--- app/src/main/java/com/alanford/carpediem/MainActivity.kt
+++ app/src/main/java/com/alanford/carpediem/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.alanford.carpediem
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
+import com.alanford.carpediem.data.Quote
 import com.alanford.carpediem.networking.Networking
+import com.alanford.carpediem.notifications.AUTHOR
+import com.alanford.carpediem.notifications.QUOTE_ID
+import com.alanford.carpediem.notifications.QUOTE_TEXT
+import com.alanford.carpediem.quotedetails.QuoteDetailFragmentArgs
+import com.alanford.carpediem.quotelist.QuoteListFragmentDirections
 import com.alanford.carpediem.repository.FavoriteQuotesRepository
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
@@ -26,12 +39,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // navigation
         setupNavigationAndMenu()
 
+        // networking
         Networking.initialize()
 
+        // database
         FavoriteQuotesRepository.initialize(this)
 
+        // notifications
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(
+                getString(R.string.quote_notification_channel_id),
+                getString(R.string.notification_channel_name)
+            )
+        }
+        handleNewIntent(intent)
+    }
+
+    private fun handleNewIntent(intent: Intent?) {
+        intent?.extras?.let {
+            val theQuoteText = it.getString(QUOTE_TEXT)
+            val theAuthor = it.getString(AUTHOR)
+            val theId = it.getString(QUOTE_ID)
+
+            if (theAuthor != null && theQuoteText != null && theId != null) {
+                val action = QuoteListFragmentDirections.actionQuoteListFragmentToQuoteFragment(
+                    Quote(
+                        id = theId,
+                        quoteText = theQuoteText,
+                        author = theAuthor
+                    )
+                )
+                val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+                val navController = host.navController
+                navController.navigate(action)
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        // called when we get a notification and the app is open but in the background
+        handleNewIntent(intent)
     }
 
     private fun setupNavigationAndMenu() {
@@ -86,5 +137,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.notification_channel_description)
+
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager?.createNotificationChannel(notificationChannel)
+        }
     }
 }
